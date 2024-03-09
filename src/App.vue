40359c06183f631ef3e6d06a0649e1fd8dd9e6b6
Jonathan Ato Markin <jona10711@gmail.com>
2024-03-09 11:12:31 +0100

    PrimeVue setup

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue'
 import MenuBar from './components/MenuBar.vue'
 
+const value = ref(['apple', 'banana', 'cherry'])
+
 </script>
 
 <template>
@@ -10,10 +12,9 @@ import MenuBar from './components/MenuBar.vue'
 
   <header>
     <MenuBar />
-    <v-icon :icon="mdiAccount" />
-    <v-chip>
-      Chip
-    </v-chip>
+    <div class="card p-fluid">
+      <Chips v-model="value" />
+    </div>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
--- src/main.js
+++ src/main.js
@@ -1,32 +1,20 @@
 import './assets/main.css'
-import '@mdi/font/css/materialdesignicons.css'
-
+import 'primevue/resources/themes/aura-light-green/theme.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
 
 
 import App from './App.vue'
 import router from './router'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-    components,
-    directives,
-    icons: {
-        defaultSet: 'mdi',
-    },
-})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
+app.use(PrimeVue)
 
 
 app.mount('#app')
